5ec7e371c0e4a64c7e246255fa18186390e807d9
dayane07 <dayaanez.9008@gmail.com>
2025-10-01 20:43:51 -0300

    eh_par

---
+++ exer05.py
@@ -0,0 +1,8 @@
+def eh_par(numero):
+    return numero % 2 == 0
+
+resultado_par = eh_par(5)
+print(f"O numero 5 é par? {resultado_par}")
+
+resultado_impar = eh_par(6)
+print(f"O numero 6 é par? {resultado_impar}")
